4e44d520bb740c8c82918357a98619c9dde8485c
Thomas R. Dial <dialtr@gmail.com>
2018-01-19 21:31:06 -0500

    moved #pragmas to main

--- config.h
+++ config.h
@@ -7,21 +7,6 @@
 #ifndef CONFIG_H_INCLUDED_
 #define CONFIG_H_INCLUDED_
 
-// Here, we are configuring various settings on the PIC18. The most important
-// setting to note here is 'OSC', which we set to 'HS'. This configures the
-// PIC18 to use an external, high-speed crystal oscillator. The frequency of
-// the oscillator used in this system is 8 MHZ, and we must define this value
-// below in _XTAL_FREQ as this symbol is used in other calculations.
-
-#pragma config OSC = HS
-#pragma config MCLRE = OFF
-#pragma config PWRT = OFF
-#pragma config PBADEN = OFF
-#pragma config WDT = OFF
-#pragma config DEBUG = OFF
-#pragma config LVP = OFF
-#pragma config BOREN = 0
-
 // The master clock runs at 8 MHZ, as described above.
 #define _XTAL_FREQ 8000000
 
--- main.c
+++ main.c
@@ -14,9 +14,27 @@
 #include "midi.h"
 #include "status.h"
 
+// Here, we are configuring various settings on the PIC18. The most important
+// setting to note here is 'OSC', which we set to 'HS'. This configures the
+// PIC18 to use an external, high-speed crystal oscillator. The frequency of
+// the oscillator used in this system is 8 MHZ, and we must define this value
+// below in _XTAL_FREQ as this symbol is used in other calculations.
+
+#pragma config OSC = HS
+#pragma config MCLRE = OFF
+#pragma config PWRT = OFF
+#pragma config PBADEN = OFF
+#pragma config WDT = OFF
+#pragma config DEBUG = OFF
+#pragma config LVP = OFF
+#pragma config BOREN = 0
+
+
+
 // Perform initial system initialization.
 char init() {
     // TODO(tdial): Implement
+    return 0;
 }
 
 // Report error state using a system peripheral
@@ -39,6 +57,5 @@ void main(void) {
     
     for (;;) {
         loop();
-    }
-    return;
+    }    
 }
