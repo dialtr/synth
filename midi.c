9675b424e2f9482e96369d76ead110d8a2886d38
Thomas R. Dial <dialtr@gmail.com>
2018-01-20 00:43:59 -0500

    work on callback registration and invocation for system realtime events

--- midi.c
+++ midi.c
@@ -42,6 +42,26 @@ static char g_data_byte_counter = 0;
 
 
 /*
+ * Callback table
+ */
+
+// The null event callback is used by default for all events.
+static void null_event_cb(char a, char b) {
+    // TODO(tdial): Increment global debugging/event counter.
+    // Intentionally empty.
+}
+
+static midi_event_callback_t invoke_sys_realtime_timing_clock = null_event_cb;
+static midi_event_callback_t invoke_sys_realtime_reserved_f9 = null_event_cb;
+static midi_event_callback_t invoke_sys_realtime_seq_start = null_event_cb;
+static midi_event_callback_t invoke_sys_realtime_seq_continue = null_event_cb;
+static midi_event_callback_t invoke_sys_realtime_seq_stop = null_event_cb;
+static midi_event_callback_t invoke_sys_realtime_reserved_fd = null_event_cb;
+static midi_event_callback_t invoke_sys_realtime_active_sense = null_event_cb;
+static midi_event_callback_t invoke_sys_realtime_reset = null_event_cb;
+
+
+/*
  * Internal APIs
  */
 
@@ -50,24 +70,40 @@ static status_t rx_status_sys_realtime_byte(char byte) {
     g_status_byte = byte;
     switch (byte) {
         case SYS_REALTIME_TIMING_CLOCK:
+            invoke_sys_realtime_timing_clock(0, 0);
             break;
-        case SYS_REALTIME_RESERVED_F9:            
+            
+        case SYS_REALTIME_RESERVED_F9:
+            invoke_sys_realtime_reserved_f9(0, 0);
             break;
+            
         case SYS_REALTIME_SEQ_START:
+            invoke_sys_realtime_seq_start(0 ,0);
             break;
+            
         case SYS_REALTIME_SEQ_CONTINUE:
+            invoke_sys_realtime_seq_continue(0, 0);
             break;
+            
         case SYS_REALTIME_SEQ_STOP:
+            invoke_sys_realtime_seq_stop(0, 0);
             break;
+            
         case SYS_REALTIME_RESERVED_FD:
+            invoke_sys_realtime_reserved_fd(0, 0);
             break;
+            
         case SYS_REALTIME_ACTIVE_SENSE:
+            invoke_sys_realtime_active_sense(0, 0);
             break;
+            
         case SYS_REALTIME_RESET:
+            invoke_sys_realtime_reset(0, 0);
             break;
     }
     return 1;
 }
+
 
 // Process a "system common" status byte (0 or more data bytes follow.)
 static status_t rx_status_sys_common_byte(char byte) {
@@ -75,11 +111,13 @@ static status_t rx_status_sys_common_byte(char byte) {
     return 0;
 }
 
+
 // Process a "channel" status byte. (1 or 2 data bytes follow.)
 static status_t rx_status_channel_byte(char byte) {
     g_status_byte = byte;    
     return 0;
 }
+
 
 // Process a trailing data byte.
 static status_t rx_data_byte(char byte) {
@@ -90,13 +128,16 @@ static status_t rx_data_byte(char byte) {
  * Public APIs 
  */
 
+
 status_t midi_init() {
     return 0;
 }
 
+
 status_t midi_register_event_handler(event_type evt, midi_event_callback_t cb) {
     return 0;    
 }
+
 
 status_t midi_receive_byte(char byte) {
     /*
@@ -123,4 +164,4 @@ status_t midi_receive_byte(char byte) {
         // The byte is a regular data byte.
         return rx_data_byte(byte);
     }
-} 
+}
